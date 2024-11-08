5247893803e5f1dcdf4614bb07e14ae45eb9a9e3
KonstantinGarbers <wurstm162@gmail.com>
2024-11-08 18:10:30 +0100

    Fixed bugs and added simple test

--- src/arm64/label_resolver.rs
+++ src/arm64/label_resolver.rs
@@ -1,6 +1,9 @@
 #![allow(unused)]
 
-use std::collections::BinaryHeap;
+use std::cmp::Ord;
+use std::cmp::Reverse;
+use std::collections::{BinaryHeap, HashSet};
+use std::hash::Hash;
 use tnj::air::instructions::{builder::InstructionBuilder, BlockParamData};
 use yaxpeax_arch::{Decoder, U8Reader};
 use yaxpeax_arm::armv8::a64::{DecodeError, InstDecoder, Opcode, Operand};
@@ -9,18 +12,30 @@ use super::AArch64LifterError;
 
 /// Create basic blocks for the InstructionBuilder based off labels
 pub struct LabelResolver {
-    checkpoints: BinaryHeap<isize>,
+    checkpoints: UniqueHeap<Reverse<isize>>,
+}
+
+impl Default for LabelResolver {
+    fn default() -> Self {
+        Self::new()
+    }
 }
 
 impl LabelResolver {
-    fn new() -> Self {
+    /// Create a new LabelResolver
+    pub fn new() -> Self {
         Self {
-            checkpoints: BinaryHeap::new(),
+            checkpoints: UniqueHeap::new(),
         }
     }
 
-    // Create basic blocks based off labels
-    fn _resolve(&mut self, code: &[u8], builder: &mut InstructionBuilder, decoder: &InstDecoder) {
+    /// Create basic blocks based off labels
+    pub fn resolve(
+        &mut self,
+        code: &[u8],
+        builder: &mut InstructionBuilder,
+        decoder: &InstDecoder,
+    ) {
         self.get_checkpoints(code, decoder);
         self.create_blocks(builder);
     }
@@ -31,13 +46,13 @@ impl LabelResolver {
         code: &[u8],
         decoder: &InstDecoder,
     ) -> Result<(), AArch64LifterError> {
+        let instruction_size = 4;
         let mut reader = U8Reader::new(code);
-        let mut address: isize = -1;
+        let mut address: isize = -instruction_size;
         loop {
-            address += 1;
+            address += instruction_size;
             match decoder.decode(&mut reader) {
                 Ok(inst) => {
-                    println!("{}", inst);
                     let imm: isize = match inst.opcode {
                         Opcode::B | Opcode::BL | Opcode::Bcc(_) => {
                             self.get_pc_offset(inst.operands[0])
@@ -52,14 +67,13 @@ impl LabelResolver {
                         }
                         _ => continue,
                     };
-                    self.checkpoints.push(address + 1);
+                    self.checkpoints.push(Reverse(address + instruction_size));
                     let jump_address = imm + address;
-                    self.checkpoints.push(jump_address);
+                    self.checkpoints.push(Reverse(jump_address));
                 }
                 Err(DecodeError::ExhaustedInput) => break,
                 Err(e) => return Err(AArch64LifterError::DecodeError(e)),
             }
-            address += 1;
         }
         Ok(())
     }
@@ -75,11 +89,49 @@ impl LabelResolver {
     fn create_blocks(&mut self, builder: &mut InstructionBuilder) {
         while !self.checkpoints.is_empty() {
             let checkpoint = match self.checkpoints.pop() {
-                Some(c) => c,
+                Some(c) => c.0,
                 None => break,
             };
             let name = format!("block_{}", checkpoint);
             builder.create_block(name, Vec::<BlockParamData>::new());
         }
+    }
+}
+
+struct UniqueHeap<T>
+where
+    T: Ord + Hash + Clone,
+{
+    heap: BinaryHeap<T>,
+    set: HashSet<T>,
+}
+
+impl<T> UniqueHeap<T>
+where
+    T: Ord + Hash + Clone,
+{
+    pub fn new() -> Self {
+        Self {
+            heap: BinaryHeap::new(),
+            set: HashSet::new(),
+        }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.heap.is_empty()
+    }
+
+    pub fn push(&mut self, item: T) {
+        if self.set.insert(item.clone()) {
+            self.heap.push(item);
+        }
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        let item = self.heap.pop();
+        if let Some(ref item) = item {
+            self.set.remove(item);
+        }
+        item
     }
 }
---
+++ tests/label_resolver_simple.rs
@@ -0,0 +1,47 @@
+use std::vec;
+
+use aarch64_air_lifter::arm64::LabelResolver;
+use target_lexicon::{Aarch64Architecture, Architecture};
+use tnj::air::instructions::Blob;
+use tnj::arch::get_arch;
+use yaxpeax_arch::Arch;
+use yaxpeax_arm::armv8::a64::ARMv8;
+
+#[test]
+#[ignore]
+// Add with carry
+fn test() {
+    let bytes = [
+        0x20, 0x00, 0x02, 0xB0, // add x0, x1, x2
+        0xFF, 0xFF, 0xFF, 0x17, // b -1
+        0x00, 0x00, 0x00, 0x14, // b 0
+        0xFD, 0xFF, 0xFF, 0x17, // b -3
+        0x41, 0x00, 0x03, 0x8B, // add x1, x2, x3
+        0x02, 0x00, 0x00, 0x14, // b 2
+        0x41, 0x00, 0x03, 0x8B, // add x1, x2, x3
+    ];
+
+    let arch = get_arch(Architecture::Aarch64(Aarch64Architecture::Aarch64)).unwrap();
+    let mut blob = Blob::new(arch);
+    let mut builder = blob.insert();
+    let decoder = <ARMv8 as Arch>::Decoder::default();
+
+    let mut label_resolver = LabelResolver::new();
+    label_resolver.resolve(&bytes, &mut builder, &decoder);
+
+    // println!("{}", blob.display().to_string());
+
+    let expected: Vec<String> = vec![
+        "entry", "block_0", "block_8", "block_12", "block_16", "block_24", "block_28",
+    ]
+    .into_iter()
+    .map(String::from)
+    .collect();
+
+    let mut actual = vec![];
+    for block in blob.blocks() {
+        actual.push(block.name());
+    }
+
+    assert_eq!(expected, actual);
+}
