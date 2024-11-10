eaa080dd2da1d9a97a5a5081ff266be082071a0a
KonstantinGarbers <wurstm162@gmail.com>
2024-11-10 17:57:20 +0100

    Organized test files in directories

---
+++ tests/mod.rs
@@ -0,0 +1,17 @@
+mod label_resolver {
+    mod label_resolver_simple;
+}
+
+mod lifter {
+    mod adc_simple;
+    mod adcs_simple;
+    mod add_simple;
+    mod adds_simple;
+    mod and_simple;
+    mod asr_simple;
+    mod b_simple;
+    mod bfm_simple;
+    mod bic_simple;
+    mod csinc_simple;
+    mod sub_simple;
+}
