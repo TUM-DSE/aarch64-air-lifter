e79095b17a1509dc4f454e6c4d9e2060bdaf0682
KonstantinGarbers <wurstm162@gmail.com>
2024-12-17 16:22:54 +0100

    Refactored tests for adrp

--- tests/lifter/adrp_simple.rs
+++ tests/lifter/adrp_simple.rs
@@ -1,16 +1,32 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
-#[test]
 // Form PC-relative address to 4KB page
-fn test() {
+#[test]
+fn test_adrp_1() {
     let bytes = [
         0x00, 0x00, 0x00, 0x90, // adrp x0, pc
+    ];
+    let directives = r#"
+        #0 check: // entry block
+        #1 nextln: v37 = i64.read_reg "pc"
+        #2 nextln: v38 = i64.add v37, 0x0
+        #3 nextln: i64.write_reg v38, "x0"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None));
+}
+
+#[test]
+fn test_adrp_2() {
+    let bytes = [
         0x00, 0x00, 0x00, 0xB0, // adrp x0, pc+4096
     ];
+    let directives = r#"
+        #0 check: // entry block
+        #1 nextln: v37 = i64.read_reg "pc"
+        #2 nextln: v38 = i64.add v37, 0x1000
+        #3 nextln: i64.write_reg v38, "x0"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None));
 }
