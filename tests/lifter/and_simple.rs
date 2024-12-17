ab684b027cdc08a7e2886a1eba8a40e3cab4ff0a
KonstantinGarbers <wurstm162@gmail.com>
2024-12-17 16:32:05 +0100

    Refactored and test

--- tests/lifter/and_simple.rs
+++ tests/lifter/and_simple.rs
@@ -1,17 +1,49 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
 #[test]
 // Bitwise AND
-fn test() {
+fn test_and_1() {
     let bytes = [
         0x20, 0x04, 0x00, 0x12, // and w0, w1, #3
+    ];
+    let directives = r#"
+        #0 check: // entry block
+        #1 nextln: v37 = i32.read_reg "x1"
+        #2 nextln: v38 = i32.and v37, 0x3
+        #3 nextln: i32.write_reg v38, "x0"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None));
+}
+
+#[test]
+fn test_and_2() {
+    let bytes = [
         0x41, 0x00, 0x40, 0x92, // and x1, x2, #1
+    ];
+    let directives = r#"
+        #0 check: // entry block
+        #1 nextln: v37 = i64.read_reg "x2"
+        #2 nextln: v38 = i64.and v37, 0x1
+        #3 nextln: i64.write_reg v38, "x1"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None));
+}
+
+#[test]
+fn test_and_3() {
+    let bytes = [
         0x20, 0x10, 0x02, 0x8A, // and x0, x1, x2, LSL#4
     ];
+    let directives = r#"
+        #0 check: // entry block    
+        #1 nextln: v37 = i64.read_reg "x1"
+        #2 nextln: v38 = i64.read_reg "x2"
+        #3 nextln: v39 = i64.lshl v38, 0x4
+        #4 nextln: v40 = i64.and v37, v39
+        #5 nextln: i64.write_reg v40, "x0"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None));
 }
