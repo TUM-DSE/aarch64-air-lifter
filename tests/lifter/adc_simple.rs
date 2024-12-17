19bccb13cdcde3630d6e6d13d049816dbef944f4
KonstantinGarbers <wurstm162@gmail.com>
2024-12-17 15:37:26 +0100

    Refactored the adc tests

--- tests/lifter/adc_simple.rs
+++ tests/lifter/adc_simple.rs
@@ -1,16 +1,39 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
+
+// Add with carry
 
 #[test]
-// Add with carry
-fn test() {
+fn test_adc_1() {
     let bytes = [
         0x21, 0x00, 0x00, 0x9A, // adc x1, x1, x0
+    ];
+    let directives = r#"
+        #0 check: // entry block
+        #1 nextln:v37 = i64.read_reg "x1"
+        #2 nextln:v38 = i64.read_reg "x0"
+        #3 nextln:v39 = i1.read_reg "c"
+        #4 nextln:v40 = i64.add v37, v39
+        #5 nextln:v41 = i64.add v40, v38
+        #6 nextln:i64.write_reg v41, "x1"
+    "#;
+
+    check_instruction(bytes, directives, None);
+}
+
+#[test]
+fn test_adc_2() {
+    let bytes = [
         0x21, 0x00, 0x00, 0x1A, // adc w1, w1, w0
     ];
+    let directives = r#"
+        #0 check: // entry block
+        #1 nextln: v37 = i32.read_reg "x1"
+        #2 nextln: v38 = i32.read_reg "x0"
+        #3 nextln: v39 = i1.read_reg "c"
+        #4 nextln: v40 = i32.add v37, v39
+        #5 nextln: v41 = i32.add v40, v38
+        #6 nextln: i32.write_reg v41, "x1"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    check_instruction(bytes, directives, None);
 }
