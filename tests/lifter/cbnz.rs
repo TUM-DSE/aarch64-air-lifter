754a7446531b9b79d9816528df0d1e5244f2e695
KonstantinGarbers <wurstm162@gmail.com>
2024-12-18 13:59:46 +0100

    Refactore cbnz tests

--- tests/lifter/cbnz.rs
+++ tests/lifter/cbnz.rs
@@ -1,17 +1,48 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
-#[test]
 // Compare and branch on nonzero
-fn test() {
+#[test]
+fn test_cbnz_1() {
     let bytes = [
         0x00, 0x00, 0x00, 0xB5, // cbnz x0, pc
+    ];
+    let directives = r#"
+        check: // entry block
+        check: block_0:
+        nextln:   v37 = i64.read_reg "x0"
+        nextln:   v38 = i64.icmp.ne v37, 0x0
+        nextln:   jumpif v38, $LABEL, $LABEL 
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+#[test]
+
+fn test_cbnz_2() {
+    let bytes = [
         0xE0, 0xFF, 0xFF, 0x35, // cbnz w0, pc-1
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln: v37 = i32.read_reg "x0"
+        nextln: v38 = i32.icmp.ne v37, 0x0
+        nextln: jumpif v38, $LABEL, $LABEL
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_cbnz_3() {
+    let bytes = [
         0x20, 0x80, 0x00, 0xB5, // cbnz x0, pc+1
     ];
+    let directives = r#"
+        check: // entry block
+        nextln: v37 = i64.read_reg "x0"
+        nextln: v38 = i64.icmp.ne v37, 0x0
+        nextln: jumpif v38, $LABEL, $LABEL
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None))
 }
