58c95092193855e05af86120ddc5b7069a049d83
KonstantinGarbers <wurstm162@gmail.com>
2024-12-18 13:38:12 +0100

    Refactored the bic tests

--- tests/lifter/bic.rs
+++ tests/lifter/bic.rs
@@ -1,17 +1,56 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
-#[test]
 // Bitwise bit clear (shifted register)
-fn test() {
+#[test]
+fn test_bic_1() {
     let bytes = [
         0x41, 0x08, 0x23, 0x0A, // bic w1, w2, w3, lsl #2
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln: v37 = i32.read_reg "x2"
+        nextln: v38 = i32.read_reg "x3"
+        nextln: v39 = i32.lshl v38, 0x2
+        nextln: v40 = i32.not v39
+        nextln: v41 = i32.and v37, v40
+        nextln: i32.write_reg v41, "x1"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_bic_2() {
+    let bytes = [
         0x41, 0x0C, 0x23, 0x8A, // bic x1, x2, x3, lsl #3
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln: v37 = i64.read_reg "x2"
+        nextln: v38 = i64.read_reg "x3"
+        nextln: v39 = i64.lshl v38, 0x3
+        nextln: v40 = i64.not v39
+        nextln: v41 = i64.and v37, v40
+        nextln: i64.write_reg v41, "x1"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_bic_3() {
+    let bytes = [
         0x41, 0x04, 0xA3, 0x8A, // bic x1, x2, x3, asr #1
     ];
+    let directives = r#"
+        check: // entry block
+        nextln: v37 = i64.read_reg "x2"
+        nextln: v38 = i64.read_reg "x3"
+        nextln: v39 = i64.ashr v38, 0x1
+        nextln: v40 = i64.not v39
+        nextln: v41 = i64.and v37, v40
+        nextln: i64.write_reg v41, "x1"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None))
 }
