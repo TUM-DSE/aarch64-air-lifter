c71f041640386cde5bd8fde68e4983923ff5ac10
KonstantinGarbers <wurstm162@gmail.com>
2024-12-19 15:27:15 +0100

    Refactored the ldar tests

--- tests/lifter/ldar.rs
+++ tests/lifter/ldar.rs
@@ -1,17 +1,49 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
 #[test]
-// Load register
-fn test() {
+fn test_ldar_1() {
     let bytes = [
         0x21, 0xFC, 0xDF, 0x88, // ldar w1, [x1]
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "x1"
+        nextln:  v38 = i64.add v37, 0x0
+        nextln:  v39 = i32.load v38
+        nextln:  i32.write_reg v39, "x1"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_ldar_2() {
+    let bytes = [
         0xE1, 0xFF, 0xDF, 0xC8, // ldar x1, [sp]
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "sp"
+        nextln:  v38 = i64.add v37, 0x0
+        nextln:  v39 = i64.load v38
+        nextln:  i64.write_reg v39, "x1"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_ldar_3() {
+    let bytes = [
         0x21, 0xFC, 0xDF, 0xC8, // ldar x1, [x1]
     ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "x1"
+        nextln:  v38 = i64.add v37, 0x0
+        nextln:  v39 = i64.load v38
+        nextln:  i64.write_reg v39, "x1"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None))
 }
