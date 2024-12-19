dee0e29529d24f93ed8dbb5ca4520a3ead84c46d
KonstantinGarbers <wurstm162@gmail.com>
2024-12-19 16:45:04 +0100

    Refactored tests for sdiv

--- tests/lifter/sdiv.rs
+++ tests/lifter/sdiv.rs
@@ -1,16 +1,34 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
-#[test]
 // Signed divide
-fn test() {
+#[test]
+fn test_sdiv_1() {
     let bytes = [
         0x20, 0x0C, 0xC2, 0x9A, // sdiv x0, x1, x2
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "x1"
+        nextln:  v38 = i64.read_reg "x2"
+        nextln:  v39 = i64.idiv v37, v38
+        nextln:  i64.write_reg v39, "x0"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_sdiv_2() {
+    let bytes = [
         0x20, 0x0C, 0xC2, 0x1A, // sdiv w0, w1, w2
     ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i32.read_reg "x1"
+        nextln:  v38 = i32.read_reg "x2"
+        nextln:  v39 = i32.idiv v37, v38
+        nextln:  i32.write_reg v39, "x0"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None))
 }
