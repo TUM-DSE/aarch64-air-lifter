0d23a3433432512e7f6e02b51b5389902da2abe4
KonstantinGarbers <wurstm162@gmail.com>
2024-12-19 16:19:20 +0100

    Refactored the tests for madd

--- tests/lifter/madd.rs
+++ tests/lifter/madd.rs
@@ -1,16 +1,49 @@
 use aarch64_air_lifter::arm64::AArch64Lifter;
 use aarch64_air_lifter::Lifter;
 
+use crate::common::lib::check_instruction;
+
 #[test]
 // Multiply-add
 fn test() {
-    let bytes = [
-        0x20, 0x0c, 0x02, 0x9B, // madd x0, x1, x2, x3
-        0x20, 0x0c, 0x02, 0x1B, // madd w0, w1, w2, w3
-    ];
+    let bytes = [];
 
     let lifter = AArch64Lifter;
     let blob = lifter.lift(&bytes, &[]).unwrap();
 
     println!("{}", blob.display());
+}
+
+#[test]
+fn test_madd_1() {
+    let bytes = [
+        0x20, 0x24, 0xC2, 0x1A, // lsr w0, w1, w2
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i32.read_reg "x1"
+        nextln:  v38 = i32.read_reg "x2"
+        nextln:  v39 = i32.and v38, 0x1f
+        nextln:  v40 = i32.lshr v37, v39
+        nextln:  i32.write_reg v40, "x0"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_madd_2() {
+    let bytes = [
+        0x20, 0x24, 0xC2, 0x9A, // lsr x0, x1, x2
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "x1"
+        nextln:  v38 = i64.read_reg "x2"
+        nextln:  v39 = i64.and v38, 0x3f
+        nextln:  v40 = i64.lshr v37, v39
+        nextln:  i64.write_reg v40, "x0"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
 }
