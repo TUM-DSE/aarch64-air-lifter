e42b02b2a45319c3eca8176613fbec8ed34ff26d
KonstantinGarbers <wurstm162@gmail.com>
2024-12-19 15:19:28 +0100

    Refactored the tests for eor

--- tests/lifter/eor.rs
+++ tests/lifter/eor.rs
@@ -1,16 +1,32 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
-#[test]
 // bit-wise exclusive OR
-fn test() {
+#[test]
+fn test_eor_1() {
     let bytes = [
         0x20, 0x00, 0x7E, 0xD2, // eor x0, x1, #0x4
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "x1"
+        nextln:  v38 = i64.xor v37, 0x4
+        nextln:  i64.write_reg v37, "x0"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_eor_2() {
+    let bytes = [
         0x41, 0x00, 0x1D, 0x52, // eor w1, w2, #0x8
     ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i32.read_reg "x2"
+        nextln:  v38 = i32.xor v37, 0x8
+        nextln:  i32.write_reg v37, "x1"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None))
 }
