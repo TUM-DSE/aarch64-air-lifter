b0e0a45e7c9f4c6b6c4033571fef4101b9e73781
KonstantinGarbers <wurstm162@gmail.com>
2024-12-19 16:15:38 +0100

    Refactored the tests for lslv

--- tests/lifter/lslv.rs
+++ tests/lifter/lslv.rs
@@ -1,16 +1,36 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
-#[test]
 // Logical shift left variable
-fn test() {
+#[test]
+fn test_lslv_1() {
     let bytes = [
         0x20, 0x20, 0xC2, 0x9A, // lslv x0, x1, x2
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "x1"
+        nextln:  v38 = i64.read_reg "x2"
+        nextln:  v39 = i64.and v38, 0x3f
+        nextln:  v40 = i64.lshl v37, v39
+        nextln:  i64.write_reg v40, "x0"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_lslv_2() {
+    let bytes = [
         0x20, 0x20, 0xC2, 0x1A, // lslv w0, w1, w2
     ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i32.read_reg "x1"
+        nextln:  v38 = i32.read_reg "x2"
+        nextln:  v39 = i32.and v38, 0x1f
+        nextln:  v40 = i32.lshl v37, v39
+        nextln:  i32.write_reg v40, "x0"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None))
 }
