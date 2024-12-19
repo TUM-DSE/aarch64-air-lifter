71d8ec078aad96daee2997b47f48ba2ba3b51b9f
KonstantinGarbers <wurstm162@gmail.com>
2024-12-19 16:11:27 +0100

    Refactored the tests for ldur

--- tests/lifter/ldur.rs
+++ tests/lifter/ldur.rs
@@ -1,17 +1,51 @@
-use aarch64_air_lifter::arm64::AArch64Lifter;
-use aarch64_air_lifter::Lifter;
+use crate::common::lib::check_instruction;
 
-#[test]
 // Load register (unscaled)
-fn test() {
+#[test]
+fn test_ldur_1() {
     let bytes = [
         0xE1, 0x73, 0x41, 0xB8, // ldur w1, [sp, #23]
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "sp"
+        nextln:  v38 = i64.add v37, 0x17
+        nextln:  v39 = i32.load v38
+        nextln:  v40 = i64.zext_i32 v39
+        nextln:  i32.write_reg v40, "x1"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_ldur_2() {
+    let bytes = [
         0xE1, 0x73, 0x41, 0xF8, // ldur x1, [sp, #23]
+    ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "sp"
+        nextln:  v38 = i64.add v37, 0x17
+        nextln:  v39 = i64.load v38
+        nextln:  i64.write_reg v39, "x1"
+    "#;
+
+    assert!(check_instruction(bytes, directives, None))
+}
+
+#[test]
+fn test_ldur_3() {
+    let bytes = [
         0x41, 0x40, 0x40, 0xF8, // ldur x1, [x2, #4]
     ];
+    let directives = r#"
+        check: // entry block
+        nextln:  v37 = i64.read_reg "x2"
+        nextln:  v38 = i64.add v37, 0x4
+        nextln:  v39 = i64.load v38
+        nextln:  i64.write_reg v39, "x1"
+    "#;
 
-    let lifter = AArch64Lifter;
-    let blob = lifter.lift(&bytes, &[]).unwrap();
-
-    println!("{}", blob.display());
+    assert!(check_instruction(bytes, directives, None))
 }
