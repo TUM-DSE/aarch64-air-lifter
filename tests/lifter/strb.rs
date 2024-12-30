c4ba1765b55df5b2c7cc5e72f81654186ee07407
KonstantinGarbers <wurstm162@gmail.com>
2024-12-30 16:18:52 +0100

    Fixed tests for strb

--- tests/lifter/strb.rs
+++ tests/lifter/strb.rs
@@ -7,11 +7,10 @@ fn test_strb_1() {
     ];
     let directives = r#"
         check: // entry block
-        nextln:  v37 = i32.read_reg "x0"
-        nextln:  v38 = i8.trunc_i64 v37
-        nextln:  v39 = i64.read_reg "sp"
-        nextln:  v40 = i64.add v39, 0xc
-        nextln:  i8.store v38, v40
+        nextln: v37 = i32.read_reg "x0"
+        nextln: v38 = i64.read_reg "sp"
+        nextln: v39 = i64.add v38, 0xc
+        nextln: i8.store v37, v39
     "#;
 
     assert!(check_instruction(bytes, directives, None))
@@ -24,11 +23,10 @@ fn test_strb_2() {
     ];
     let directives = r#"
         check: // entry block
-        nextln:  v37 = i32.read_reg "x0"
-        nextln:  v38 = i8.trunc_i64 v37
-        nextln:  v39 = i64.read_reg "x1"
-        nextln:  v40 = i64.add v39, 0x2
-        nextln:  i8.store v38, v40
+        nextln: v37 = i32.read_reg "x0"
+        nextln: v38 = i64.read_reg "x1"
+        nextln: v39 = i64.add v38, 0x2
+        nextln: i8.store v37, v39
     "#;
 
     assert!(check_instruction(bytes, directives, None))
@@ -41,14 +39,13 @@ fn test_strb_3() {
     ];
     let directives = r#"
         check: // entry block
-        nextln:  v37 = i32.read_reg "x1"
-        nextln:  v38 = i8.trunc_i64 v37
-        nextln:  v39 = i64.read_reg "x1"
-        nextln:  v40 = i32.read_reg "x1"
-        nextln:  v41 = i32.trunc_i64 v40
-        nextln:  v42 = i64.sext_i32 v41
-        nextln:  v43 = i64.add v39, v42
-        nextln:  i8.store v38, v43
+        nextln: v37 = i32.read_reg "x1"
+        nextln: v38 = i64.read_reg "x1"
+        nextln: v39 = i32.read_reg "x1"
+        nextln: v40 = i32.trunc_i64 v39
+        nextln: v41 = i64.sext_i32 v40
+        nextln: v42 = i64.add v38, v41
+        nextln: i8.store v37, v42
     "#;
 
     assert!(check_instruction(bytes, directives, None))
