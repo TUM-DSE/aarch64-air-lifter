e4cfc7d99f5518fdefe0677ed547c7e6b802e7c0
Martin Fink <martin@finkmartin.com>
2025-03-26 19:10:50 +0100

    remove unneeded files

--- tests/lifter/insts/generated/generated/ldrsw.rs
+++
@@ -1,8 +0,0 @@
-#![cfg_attr(rustfmt, rustfmt_skip)]// ⚠️ Automatically generated file, do not edit! ⚠️
-
-use crate::lifter::yaml_tests::run_test_from_yaml;
-
-#[test]
-pub fn test_test_ldrsw_1() {
-    run_test_from_yaml("tests/lifter/insts/tests/ldrsw.yaml", "test_ldrsw_1");
-}
